7a96d2d9a215149b8a443cd463c09a3c43069024
IrtazaHussain11223 <irtazah999@gmail.com>
2026-03-20 10:37:47 +0500

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -6,3 +6,14 @@
 --Will implement advanced queries and optimize database operations.
 --Will ensure efficient communication between backend APIs and database.
 --Will handle data integrity, validation, and performance improvements.
+
+--Database Design Details:
+
+--The system database will consist of multiple tables including:
+--Users (Login), Patients, Staff, Appointments, Admissions, Wards,
+--NursingTasks, Prescriptions, Medicines, Inventory,
+--LabTests, LabReports, Billing, EmergencyRecords.
+
+--Tables will be connected using primary and foreign keys.
+--Normalization will be applied to reduce redundancy.
+--Joins will be used to manage relationships and retrieve combined data across modules.
