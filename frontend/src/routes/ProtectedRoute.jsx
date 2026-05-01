03f70c2f05d868082459efad0ef9819f486921c9
Ali Raza <alirazamehar0321@gmail.com>
2026-05-01 21:19:06 +0500

    Add protected routes for doctor pages

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,1 +1,25 @@
 
+ import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
+
+import DoctorLayout from "./pages/doctor/DoctorLayout";
+import DoctorDashboard from "./pages/doctor/DoctorDashboard";
+import Appointments from "./pages/doctor/Appointments";
+import Patients from "./pages/doctor/Patients";
+import Prescriptions from "./pages/doctor/Prescriptions";
+import Pharmacy from "./pages/doctor/Pharmacy";
+ 
+<Route
+  path="/doctor"
+  element={
+    <ProtectedRoute role="doctor">
+      <DoctorLayout />
+    </ProtectedRoute>
+  }
+>
+  <Route index element={<DoctorDashboard />} />
+  <Route path="appointments" element={<Appointments />} />
+  <Route path="patients" element={<Patients />} />
+  <Route path="prescriptions" element={<Prescriptions />} />
+  <Route path="pharmacy" element={<Pharmacy />} />
+</Route>
